194737ff752acd3d9f1c1a03c0c303d373b7f6fa
IWasMouseCop <spencer.dasilva993@gmail.com>
2025-02-19 16:19:51 -0500

    Initial commit

---
+++ test/Test_Lab3.py
@@ -0,0 +1,21 @@
+import unittest
+from app.Lab3 import circle_area, trapezium_area, ellipse_area, rhombus_area
+
+
+class TestShapes(unittest.TestCase):
+
+    def setUp(self):
+        print("Setup: Preparing tests...")
+
+    def tearDown(self):
+        print("Teardown: Cleaning up after tests...")
+
+    def test_circle_area_valid(self):
+        self.assertAlmostEqual(circle_area(3), 28.274333882308138)
+
+    def test_circle_area_invalid(self):
+        with self.assertRaises(ValueError):
+            circle_area(-1)
+
+if __name__ == "__main__":
+    unittest.main()
